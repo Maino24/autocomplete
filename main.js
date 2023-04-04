3098186c6a0b721c6b2f4f95e8f3bfa46c5501e6
Gabriele Mainetti <mg02.gabriele@gmail.com>
2023-04-04 16:18:04 +0200

    ciclo for

--- main.js
+++ main.js
@@ -20,3 +20,24 @@ xhttp.onreadystatechange = function(){
 
 xhttp.open("GET", "data/regioni.json", true);
 xhttp.send();
+
+let sortedRegioni = item;
+
+input.addEventListener("keyup", (e) =>{
+    
+
+
+    for(let i of sortedRegioni) {
+        if(i.toLowerCase().startWith(input.ariaValueMax.toLowerCase()) && input.value !=""){
+            let listItem = document.createElement("li");
+            listItem.classList.add("list-items");
+            listItem.style.cursor = "pointer";
+            listItem.setAttribute("onclick", "displayRegioni('" + i + "')");
+
+            let word = "<b>" + i.substr(0, input.value.length) + "</b>";
+            word += i.substr(input.value.length);
+            listItem.innerHTML = word;
+            document.querySelector(".list").appendChild(listItem);
+        }
+    }
+})
