95e32544b96f3a8c7eb903a91672af04245adf9c
Gabriele Mainetti <mg02.gabriele@gmail.com>
2023-04-04 16:08:24 +0200

    onreadystatechange function

--- main.js
+++ main.js
@@ -1,3 +1,19 @@
 var xhttp = new XMLHttpRequest;
 let input = document.getElementById('input');
 
+xhttp.onreadystatechange = function(){
+    if(this.readyState == 4 && this.status == 200){
+        var response = JSON.parse(xhttp.responseText);
+
+        var regioni = response.regioni;
+
+        for(let i = 0; i < regioni.length; i++){
+            item += `<li id="regioniItem">${regioni[i].nome} </li>`
+        }
+
+        document.getElementById('regioni').innerHTML = item;
+    }
+};
+
+xhttp.open("GET", "data/regioni.json", true);
+xhttp.send();
